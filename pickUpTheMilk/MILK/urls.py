d140c0f5774384a32cc4938d67ce5a58f0bdb715
fahsan13 <2242114A@student.gla.ac.uk>
2017-02-23 13:44:54 +0000

    setup URLS for login, register, sitemap, contact, about and create-group pages. Added new urls.py file to MILK folder; urls.py in /pickUpTheMilk/ uses the data from the new file.

---
+++ pickUpTheMilk/MILK/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from MILK import views
+
+urlpatterns = [
+    url(r'^$', views.home, name= 'home'),
+    url(r'^login/', views.login, name= 'login'),
+    url(r'^register/', views.register, name= 'register'),
+    url(r'^sitemap/', views.sitemap, name= 'sitemap'),
+    url(r'^contact/', views.contact, name= 'contact'),
+    url(r'^about/', views.about, name= 'about'),
+    url(r'^create-group/', views.creategroup, name= 'sitemap'),
+]
