cd96e5a5dbd83314ed5b2d485ef13a2f515767d6
fahsan13 <2242114A@student.gla.ac.uk>
2017-03-02 14:19:48 +0000

    Refactored user auth to make use of django-registration-redux package

--- pickUpTheMilk/MILK/urls.py
+++ pickUpTheMilk/MILK/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from MILK import views
 
 urlpatterns = [
     url(r'^$', views.home, name= 'home'),
-    url(r'^login/', views.user_login, name= 'login'),
-    url(r'^register/', views.register, name= 'register'),
+    # url(r'^login/', views.user_login, name= 'login'),
+    # url(r'^register/', views.register, name= 'register'),
     url(r'^sitemap/', views.sitemap, name= 'sitemap'),
     url(r'^contact/', views.contact, name= 'contact'),
     url(r'^about/', views.about, name= 'about'),
     url(r'^create-group/', views.creategroup, name= 'creategroup'),
-    url(r'^logout/', views.user_logout, name='logout'),
+    # url(r'^logout/', views.user_logout, name='logout'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 
     # Work in progress
     url(r'^profileID=(?P<userID>[\w\-]+)/$', views.userprofile, name= 'profile'),
-
 ]
--- pickUpTheMilk/MILK/views.py
+++ pickUpTheMilk/MILK/views.py
@@ -15,68 +15,6 @@ def home(request):
     response = render(request, 'MILK/home.html', {})
     return response
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        # Check if valid username/pw combo
-        user = authenticate(username=username, password=password)
-
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-            else:
-                # Inactive account, so no login
-                return HttpResponse("Your account is disabled.")
-        else:
-            # Invalid login details
-            print("Invalid login details:{0},{1}".format(username, password))
-            return HttpResponse("Invalid login details supplied")
-
-    else:
-        # Request not a HTTP POST, so display login form
-        return render(request, 'MILK/login.html', {})
-
-@login_required
-def user_logout(request):
-    logout(request)
-    # Take user back to homepage
-    return HttpResponseRedirect(reverse('home'))
-
-def register(request):
-    # Boolean to track registration status, will become True
-    # if user is successfully registered.
-    registered = False
-
-    # HTTP request type must be POST
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-
-        if user_form.is_valid():
-            # Save their form to the database
-            user = user_form.save()
-
-            # Hash and save their password
-            user.set_password(user.password)
-            user.save()
-
-            registered = True
-        else:
-            # Invalid form - print errors
-            print(user_form.errors)
-
-    else:
-        # Not a HTTP POST, so render form using ModelForm instances
-        user_form = UserForm()
-
-    return render(request,
-                    'MILK/register.html',
-                    {'user_form': user_form,
-                    'registered': registered})
-
-
 def sitemap(request):
     response = render(request, 'MILK/sitemap.html', {})
     return response
