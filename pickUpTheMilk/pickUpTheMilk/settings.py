69bb1e3c24576b969f0defa00873dc986cec2acc
theMicroGirl <e.grabowiecka@outlook.com>
2017-03-10 11:57:20 +0000

    python anywhere settings

--- pickUpTheMilk/pickUpTheMilk/settings.py
+++ pickUpTheMilk/pickUpTheMilk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'z11r3z=org)5=c)$#h!#tx53_9e(yp)_+odl#-7a#=%8r=l=h!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com'] #CAN HOST ON PYTHONANYWHERE
 
 
 # Application definition
