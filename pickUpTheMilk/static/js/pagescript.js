953a72daa31a8e9bca023ba059097831c781cc4d
theMicroGirl <e.grabowiecka@outlook.com>
2017-03-14 18:16:31 +0000

    navbar

--- pickUpTheMilk/static/js/pagescript.js
+++
@@ -1,7 +0,0 @@
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 30) {
-        $('.menu').addClass('fixed');
-    } else {
-        $('.menu').removeClass('fixed');
-    }
-});
